e58a608d2bee09c795db2037f33f14649ee24b58
liuyongchao <v-yongliu@microsoft.com>
2021-11-17 14:51:15 +0800

    modify

---
+++ HelloDotnetsB/Controllers/WeatherForecastController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Consul;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace HelloDotnetsB.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        public readonly ConsulSetting _consulOption;
+        private readonly IHttpClientFactory _httpClientFactory;
+        public WeatherForecastController(IOptions<ConsulSetting> consulOption, IHttpClientFactory httpClientFactory)
+        {
+            _consulOption = consulOption.Value;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            using (var consulClient = new ConsulClient(a => a.Address = new Uri(_consulOption.Address)))
+            {
+                var services = consulClient.Catalog.Service("service-a").Result.Response;
+                if (services != null && services.Any())
+                {
+                    // 模拟随机一台进行请求，这里只是测试，可以选择合适的负载均衡框架
+                    var service = services.ElementAt(new Random().Next(services.Count()));
+
+                    var client = _httpClientFactory.CreateClient();
+                    var response = await client.GetAsync($"http://{service.ServiceAddress}:{service.ServicePort}/WeatherForecast/Seattle");
+                    var result = await response.Content.ReadAsStringAsync();
+                    return result;
+                }
+            }
+            return "Not Found";
+        }
+    }
+}
---
+++ HelloDotnetsB/Startup.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+
+namespace HelloDotnetsB
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.Configure<ConsulSetting>(Configuration.GetSection(nameof(ConsulSetting)));
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "HelloDotnetsB", Version = "v1" });
+            });
+            services.AddHealthChecks();
+            services.AddHttpClient();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHostApplicationLifetime lifetime, IOptions<ConsulSetting> consulSetting)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HelloDotnetsB v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.RegisterConsul(lifetime, consulSetting.Value);
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health");
+            });
+        }
+    }
+}
